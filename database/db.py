b5c6f42f0725a3a0a27b735fad157a8e72448e7c
lakshya6935 <lakshyasharma1226@gmail.com>
2026-07-04 00:41:47 +0530

    Add DB port to MySQL connection

--- database/db.py
+++ database/db.py
@@ -1,20 +1,22 @@
 import mysql.connector
-from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
 
 
 def get_connection(database=True):
     if database:
-        return mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME
-        )
-    return mysql.connector.connect(
+      return mysql.connector.connect(
         host=DB_HOST,
+        port=DB_PORT,
         user=DB_USER,
-        password=DB_PASSWORD
-    )
+        password=DB_PASSWORD,
+        database=DB_NAME
+)
+  return mysql.connector.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USER,
+    password=DB_PASSWORD
+)
 
 
 def init_database():
